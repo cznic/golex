20244202f56436b2498ee6a4314a3ffac3c72791
My Name <myname@tux64.net.upc.cz>
2011-06-04 19:03:23 +0200

    Documented %yyx macros.

--- doc.go
+++ doc.go
@@ -11,7 +11,7 @@
 //
 // Options:
 //	-DFA            print the DFA to stdout and quit
-//	-nodfaopt		disable DFA optimization - don't use this for production code
+//	-nodfaopt       disable DFA optimization - don't use this for production code
 //	-o fname        write to file `fname`, default is `lex.yy.go`
 //	-t              write to stdout
 //	-v              write some scanner statistics to stderr
@@ -63,19 +63,31 @@
 //	    return current
 //	}
 //	
+//	//    %yyc is a "macro" to access the "current" character.
+//	//
+//	//    %yyn is a "macro" to move to the "next" character.
+//	//
+//	//    %yyb is a "macro" to return the begining-of-line status (a bool typed value).
+//	//        It is used for patterns like `^re`.
+//	//        Example: %yyb prev == 0 || prev == '\n'
+//	//
+//	//    %yyt is a "macro" to return the top/current start condition (an int typed value).
+//	//        It is used when there are patterns with conditions like `<cond>re`.
+//	//        Example: %yyt startCond
+//
 //	func main() { // This left brace is closed by *1
 //	    c := getc() // init
 //	%}
 //	
 //	%yyc c
 //	%yyn c = getc()
-//	
+//
 //	D   [0-9]+
 //	
 //	%%
 //	    buf = buf[:0]   // Code before the first rule is executed before every scan cycle (state 0 action)
 //	
-//	[ \t\n\r]+         // Ignore
+//	[ \t\n\r]+          // Ignore whitespace
 //	
 //	{D}                 fmt.Printf("int %q\n", buf)
 //	
