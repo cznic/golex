4aad2c4e8945f0e047d3c767497385b55b951aa1
jnml <0xjnml@gmail.com>
2013-02-09 14:51:16 +0100

    cc: WIP snaphot - Save a potential bug
    Is there a bug in package testing? Type 'make' and see a reported
    test fail source to be from a completely different file.
    
    $ go version
    go version devel +aee6d7fe395a Sat Jan 26 18:16:43 2013 -0800
    linux/amd64
    $

--- cc/all_test.go
+++ cc/all_test.go
@@ -5,9 +5,40 @@
 package cc
 
 import (
+	"bytes"
 	"testing"
 )
 
-func Test(t *testing.T) {
-	t.Fatal("TODO")
+func TestScanPeekNext(t *testing.T) {
+	const N = 1
+	b := make([]byte, N)
+	for i := range b {
+		b[i] = byte(i + 1)
+	}
+	for n := 0; n < len(b); n++ {
+		b = b[:n]
+		for sz := 0; sz <= len(b); sz++ {
+			r := bytes.NewReader(b)
+			failed := false
+			l := newLexer(
+				"file",
+				r,
+				func(file string, line, col int, msg string, args ...interface{}) bool {
+					failed = true
+					return true
+				},
+				make([]byte, sz),
+			)
+			for i := 1; i <= len(b)+1; i++ {
+				_ = l.peek()
+				switch {
+				case failed:
+					t.Fatal("TODO36")
+				default:
+					t.Fatal("TODO39")
+				}
+			}
+		}
+	}
+
 }
