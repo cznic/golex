f8dac809b9c0ad5a4ae8298335f9548f07b52fb8
Jan Mercl <0xjnml@gmail.com>
2015-04-07 17:49:12 +0200

    lex: Initial (WIP).

---
+++ lex/doc.go
@@ -0,0 +1,17 @@
+// Copyright (c) 2015 The golex Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package lex is a run time library for golex[0] generated lexical
+// analyzers[1].
+//
+// WARNING: This is a Work In Progress (2015-04-07).
+//
+// References
+//
+// -
+//
+//  [0]: http://godoc.org/github.com/cznic/golex
+//  [1]: http://en.wikipedia.org/wiki/Lexical_analysis
+//  [2]: http://golang.org/cmd/yacc/
+package lex
