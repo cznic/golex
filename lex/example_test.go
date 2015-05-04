a799639cffe3567786ea9d7de534659ec57a1cff
Jan Mercl <0xjnml@gmail.com>
2015-05-04 17:28:11 +0200

    lex: Added "do not edit" notice.

--- lex/example_test.go
+++ lex/example_test.go
@@ -1,3 +1,5 @@
+// CAUTION: Generated file - DO NOT EDIT.
+
 // Copyright (c) 2015 The golex Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
